c54e60b52a4f864480bfbbdbd0a13faed013043d
cloudgateinfra <nickhenrichs27@gmail.com>
2023-10-09 04:18:14 -0500

    updated repo

--- dev1-app-template/variables.tf
+++ dev1-app-template/variables.tf
@@ -1,4 +1,4 @@
-# all set in terraform.tfvars otherwise noted as local tfvar in .profile
+# declare vars here to have values set in terraform.tfvars or local tfvars
 
 variable "region" {
   default     = "us-west-2"
@@ -8,7 +8,7 @@ variable "vpc" {
 }
 
 variable "key_name" {
-  description = "bastion ssh key"
+  description = "jumphost ssh key"
   type        = string
 }
 
@@ -77,50 +77,90 @@ variable "task_memory" {
 }
 
 # set in .profile as local tfvar
-variable "auth_key" {
+variable "examplevar" {
+  type        = string
+}
+
+# declare vars here to have values set in terraform.tfvars or local tfvars
+
+variable "region" {
+  default     = "us-west-2"
+}
+
+variable "vpc" {
+}
+
+variable "key_name" {
+  description = "jumphost ssh key"
+  type        = string
+}
+
+variable "ami" {
+  type        = string
+}
+
+variable "hardware" {
+  type        = string
+}
+
+variable "docker_image" {
+  type        = string
+}
+
+variable "docker_tag" {
   type        = string
 }
 
 # set in .profile as local tfvar
-variable "secure_auth_key" {
+variable "docker_username" {
   type        = string
 }
 
 # set in .profile as local tfvar
-variable "logged_in_key" {
+variable "docker_password" {
+  type        = string
+  sensitive   = true
+}
+
+# set in .profile as local tfvar
+variable "db_username" {
   type        = string
 }
 
 # set in .profile as local tfvar
-variable "nonce_key" {
+variable "db_pw" {
+  description = "db password"
+  sensitive   = true
+}
+
+variable "db_name" {
+  type        = string
+}
+
+variable "sg" {
+  description = "AWS Security Group"
+}
+
+variable "domain" {
+  description = "Domain Name"
+}
+
+variable "zone" {
+  description = "AWS Zone"
+}
+
+variable "task_cpu" {
+  description = "CPU to allocate to the ECS task"
+  type        = string
+}
+
+variable "task_memory" {
+  description = "memory to allocate to the ECS task"
   type        = string
 }
 
 # set in .profile as local tfvar
-variable "auth_salt" {
-  type        = string
-}
-
-# set in .profile as local tfvar
-variable "secure_auth_salt" {
-  type        = string
-}
-
-# set in .profile as local tfvar
-variable "logged_in_salt" {
-  type        = string
-}
-
-# set in .profile as local tfvar
-variable "nonce_salt" {
-  type        = string
-}
-
-variable "wp_home" {
-  type        = string
-}
-
-variable "wp_siteurl" {
+variable "examplevar" {
   type        = string
 }
 
